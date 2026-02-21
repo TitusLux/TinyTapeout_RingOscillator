cac5cc7ed839d4bc2518f61711857164ba7c7637
TitusLux <titus3648@gmail.com>
2026-02-21 20:59:47 +0000

    Fix RO external feedback wiring and update pinout docs

--- src/tt_um_tituslux_ringosc.v
+++ src/tt_um_tituslux_ringosc.v
@@ -18,17 +18,18 @@ module tt_um_tituslux_ringosc (
 
   // All output pins must be assigned. If not used, assign to 0.
   wire en = ui_in[0]; // Use the first bit of ui_in as an enable signal for the ring oscillator
+  wire ring_in = ui_in[1]; // External feedback input
   
- (* keep *) wire n1, n2, n3, n4, n5; // Internal nodes for the ring oscillator
+ (* keep *) wire n1, n2, n3, n4, n5; // Internal nodes for the ring oscillator without internal feedback to prevent optimization
 
   // 5 - stage ring oscillator with enable control
-  assign n1 = en ? ~n5 : 1'b0;  // First inverter with enable control
+  assign n1 = en ? ~ring_in : 1'b0;  // First inverter with enable control
   assign n2 = ~n1;              // Second inverter
   assign n3 = ~n2;              // Third inverter 
   assign n4 = ~n3;              // Fourth inverter 
   assign n5 = ~n4;              // Fifth inverter 
 
-wire ro = n5;
+wire ring_out = n5;
 reg ro_sync1, ro_sync2;
 
 // Synchronize the ring oscillator output to the system clock domain
@@ -37,7 +38,7 @@ always@(posedge clk or negedge rst_n) begin
         ro_sync1 <= 1'b0;
         ro_sync2 <= 1'b0;
     end else begin
-        ro_sync1 <= ro;       // First stage of synchronization
+        ro_sync1 <= ring_out;       // First stage of synchronization
         ro_sync2 <= ro_sync1; // Second stage of synchronization
     end
 end
@@ -58,7 +59,8 @@ always@(posedge clk or negedge rst_n) begin
         window_cnt <= window_cnt + 16'd1; // Increment the window counter on each clock cycle
 
         if (ro_edge)
-            ro_count <= ro_count + 1;     // Increment the count on each edge
+            ro_count <= ro_count + 16'd1;     // Increment the count on each edge
+
         if (window_cnt == 16'hFFFF) begin
             ro_latched <= ro_count;       // Latch the count value after 65536 windows
             window_cnt <= 16'd0;              // Reset the window counter
@@ -67,12 +69,12 @@ always@(posedge clk or negedge rst_n) begin
     end
 end
 
-assign uo_out = ro_latched[7:0]; // Output the latched count value on uo_out
+assign uo_out = ro_latched[7:0];   // 8 bit latches edge count (LSBs)
 
-assign uio_out = 8'b0; // No IO outputs
-assign uio_oe = 8'b0;  // All IOs are inputs
+assign uio_out = {7'b0, ring_out}; // 
+assign uio_oe = 8'b0000_0001;  // only uio[0] is output
 
 // List all unused inputs to prevent warnings
-wire _unused = &{ena, ui_in[7:1], uio_in};
+wire _unused = &{ena, ui_in[7:2], uio_in};
 
 endmodule
